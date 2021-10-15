a46f2bbfe25e8b9d2606cfc2d81d8284b73c9ddc
shyeonnnny <ktmdgus970807@gmail.com>
2021-10-15 16:22:11 +0900

    2021.10.15

--- hellospring03/src/main/java/com/douzone/hellospring/controller/HelloController.java
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.douzone.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -9,11 +12,40 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
-	}
+	} // hellospring03/hello
 	
 	@RequestMapping("/hello2")
 	public String hello2(String name) {
 		System.out.println(name);
 		return "/WEB-INF/views/hello2.jsp";
-	}
+	} // hellospring03/hello2?n=name
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		return mav;
+	} // hellospring03/hello3?name=name
+	
+	@RequestMapping("/hello4")
+	public String hello4(String name, Model model) {
+		model.addAttribute("name",name);
+		return "/WEB-INF/views/hello3.jsp";
+	} // hellospring03/hello4?name=name
+	
+	@ResponseBody
+	@RequestMapping("/hello5")
+	public String hello5() {
+		return "<h1>hello spring</h1>";
+	} // hellospring03/hello5
+	
+
+	@RequestMapping("/hello6")
+	public String hello6() {
+		System.out.println("hello6() called");
+		return "redirect:/hello";
+	} // hellospring03/hello5
+	
+
 }
